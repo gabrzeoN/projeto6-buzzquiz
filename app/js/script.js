12fb47209de034da3b1376c8533c4f3e3eee3286
Osvaldo Júnior <osvaldojunior95@gmail.com>
2022-02-17 00:01:05 -0300

    feat: Foi criado todo o layout da tela 1, adicionado as variáveis de cores e estilos no style.css, criado as constantes com links da API no script.js

---
+++ app/js/script.js
@@ -0,0 +1,5 @@
+const OBTER_QUIZZES = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"; //GET
+const CRIAR_QUIZZES = OBTER_QUIZZES; //POST
+const OBTER_UNICO_QUIZZ = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/"; //GET
+const APAGAR_QUIZZ = OBTER_UNICO_QUIZZ; //DELETE
+const EDITAR_QUIZZ = OBTER_UNICO_QUIZZ; //PUT
