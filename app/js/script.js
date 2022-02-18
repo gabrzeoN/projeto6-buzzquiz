63fe3ac6ecdc5a66e0cf9c0f78372e2628fe6b5a
Gabriel Cari <g.cari@hotmail.com>
2022-02-18 17:22:45 -0300

    feat: Adicionado js para puxar um quizz do servidor e função para escolher uma resposta.

--- app/js/script.js
+++ app/js/script.js
@@ -3,3 +3,44 @@ const CRIAR_QUIZZES = OBTER_QUIZZES; //POST
 const OBTER_UNICO_QUIZZ = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/"; //GET
 const APAGAR_QUIZZ = OBTER_UNICO_QUIZZ; //DELETE
 const EDITAR_QUIZZ = OBTER_UNICO_QUIZZ; //PUT
+
+function chamarUmQuizz(id){
+    const promisse = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/" + id); 
+    promisse.then(renderizarUmQuizz);
+    // promisse.then((response) => {
+    //     console.log(response.data);
+
+    // });
+    // promisse.catch();
+}
+
+function renderizarUmQuizz(response){
+    const id = response.data.id;
+    const titulo = response.data.title;
+    const banner = response.data.image;
+    const perguntas = response.data.questions;
+    const niveis = response.data.levels
+
+    console.log(titulo + banner + id + perguntas + niveis);
+}
+
+function selecionarOpcao(opcaoSelecionada){
+    const caixaDaPergunta = opcaoSelecionada.parentNode.parentNode.parentNode;
+    if(!caixaDaPergunta.classList.contains("respondido")){
+        caixaDaPergunta.classList.add("respondido")
+        const opcoes = caixaDaPergunta.querySelectorAll(".opcao");
+        opcoes.forEach((opcoes) => {
+        opcoes.classList.add("opcao-nao-selecionada");
+        });
+        opcaoSelecionada.classList.remove("opcao-nao-selecionada");
+        verificarResposta(opcaoSelecionada);
+    }
+}
+
+function verificarResposta(opcaoSelecionada){
+    // if(){
+
+    // }
+}
+
+chamarUmQuizz("4368");
